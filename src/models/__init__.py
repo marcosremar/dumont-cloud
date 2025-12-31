3aa02ef3edfbcd454a05e3b1d34630103783302d
Marcos <marcosremar@gmail.com>
2025-12-31 22:39:03 +0100

    auto-claude: subtask-1-2 - Add ShareableReport to models __init__.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -4,6 +4,7 @@ from .price_history import PriceHistory, PriceAlert
 from .instance_status import InstanceStatus, HibernationEvent
 from .metrics import MarketSnapshot, ProviderReliability, PricePrediction, CostEfficiencyRanking
 from .machine_history import MachineAttempt, MachineBlacklist, MachineStats
+from .shareable_report import ShareableReport
 
 __all__ = [
     'PriceHistory',
@@ -19,4 +20,6 @@ __all__ = [
     'MachineAttempt',
     'MachineBlacklist',
     'MachineStats',
+    # Shareable reports
+    'ShareableReport',
 ]
